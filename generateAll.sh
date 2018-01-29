9eb3a797d18dc8465b6f37b3b7825466289c341a
Herve Couplet <herve.couplet@creativeone.eu>
2018-01-29 18:11:01 +0100

    gateway angular patch

--- generateAll.sh
+++ generateAll.sh
@@ -5,10 +5,15 @@
 HOME_DIR="$(pwd)"
 cd "$HOME_DIR"/app; jhipster
 cd "$HOME_DIR"/gateway-uaa; jhipster
+cd "$HOME_DIR"/gateway-angular; jhipster
 
 cd "$HOME_DIR"/app; jhipster  entity testEntity --force
 cd "$HOME_DIR"/gateway-uaa; jhipster  entity testEntity --force
+cd "$HOME_DIR"/gateway-angular; jhipster  entity testEntity --force
 
 cd "$HOME_DIR"/app; ./mvnw verify -Pprod dockerfile:build
 cd "$HOME_DIR"/gateway-uaa; ./mvnw verify -Pprod -DskipTests dockerfile:build
 
+cp -R "$HOME_DIR"/gateway-angular.patch/* "$HOME_DIR"/gateway-angular/
+cd "$HOME_DIR"/gateway-angular; yarn run cleanup && yarn run webpack:build:main
+cp -R "$HOME_DIR"/gateway-angular/build/www/* "$HOME_DIR"/docker-compose/nginx/www/
